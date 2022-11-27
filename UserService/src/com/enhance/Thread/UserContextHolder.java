0a9b2c74af78a3f5c6df99dc777d92a65458656f
srinu-tippana <svas7246@gmail.com>
2022-11-27 23:22:43 +0530

    Usercontext Example

---
+++ UserService/src/com/enhance/Thread/UserContextHolder.java
@@ -0,0 +1,32 @@
+package com.enhance.Thread;
+
+/* This class describes about  on a web server how different services can know the user
+ and how to make the user acessible to everyone*/
+public class UserContextHolder {
+
+	public static ThreadLocal<User> holder = new ThreadLocal();
+}
+class User
+{
+	
+}
+class Service1{
+	
+	//here this class will set the user into threadPool
+	public void process() {
+		User user = getUser();
+		UserContextHolder.holder.set(user);
+	}
+
+	private User getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+class Service2{
+	//Clases like this will get the user from the UserContextHolder
+	public void process() {
+		User user = UserContextHolder.holder.get();
+	}
+}
